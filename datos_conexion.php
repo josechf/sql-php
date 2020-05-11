fe7fa278fa5f16fe57dc5af4a5e62e13582ad73b
Jose Chavez <jose.chavezfl02@gmail.com>
2020-05-11 10:30:10 -0500

    datos de conexion sin POO

---
+++ datos_conexion.php
@@ -0,0 +1,7 @@
+<?php
+   $db_host="localhost";                /*direccion de la base de datos */
+   $db_nombre="prueba1";               
+   $db_usuario="root";
+   $db_pasword="";                     /*hasta aqui son datos obligatorios, dependen de quien te de la base de datos*/
+  
+?>
