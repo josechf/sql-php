8ccc3a04b557aea02547a45f131ff31173dc2947
Jose Chavez <jose.chavezfl02@gmail.com>
2020-05-19 22:09:32 -0500

    consulta MYSQLI POO y PDO POO

--- reseptionPost.php
+++ reseptionPost.php
@@ -1,31 +1,37 @@
 <?php
-
+// HACE CONSULTA MYSQLI POO y PDO POO
+  
    require('datos_conexion.php');
-   Class Accion extends Dato{
+   
+   Class Accion extends DatoPDO{
       public function Accion(){
             parent:: __construct();
       }
-      public function get_accion(){
-         $resultado=$this->conection->query('SELECT * FROM Hoja1');
-         if($resultado && $resultado->num_rows>0){
-         $producto=$resultado->fetch_all(MYSQLI_ASSOC);
-         return $producto;
-        }else{
-           echo "no se encontro ";exit;
-        }
-
+      public function get_accion($buscar){
+         $sql=('SELECT * FROM Hoja1 WHERE nombre=:m');
+         $resultado=$this->conection->prepare($sql);
+         $resultado->execute(array(':m'=>$buscar));
+         $imprime=$resultado->fetchAll(PDO::FETCH_ASSOC);
+         return $imprime; 
+         $resultado->closeCursor();
+         $this->conection=null;
       }
 
    }
-  $va=new Accion();
-  $v=$va->get_accion();
-  foreach($v as $elementos){
-     echo '<table><tr><td>';
-     echo $elementos['nombre'].'</td><td>';
-     echo $elementos['edad'].'</td><td>';
-     echo $elementos['trabajo'].'</td><td>';
-     echo $elementos['cedula'].'</td>';
-     echo '</tr></table>';
-     echo '<br><br>';
-   }
+         //-----------------------------------------------------------------------------------------------
+         /*Class Accion extends Datomysqli{
+            public function Accion(){
+                  parent:: __construct();
+            }
+            public function get_accion($buscar){
+         $resultado=$this->conection->query('SELECT * FROM Hoja1 WHERE nombre="'.$buscar.'"');
+         if($resultado && $resultado->num_rows>0){
+            $producto=$resultado->fetch_all(MYSQLI_ASSOC);
+            return $producto;
+         }else{
+            echo "no se encontro ";exit;
+         }*/
+
+      
 ?>
+
